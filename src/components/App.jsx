0e97977214cc5240ae07eb32e20ff9b6aa9529e9
Jhansi267 <regujhansi@gmail.com>
2025-05-22 05:07:09 +0530

    pending

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,20 +8,25 @@ import Profile from './Profile';
 import Login from './Login';
 import FanRankings from "../pages/FanRankings";
 import PrankEpisodes from "../pages/PrankEpisodes";
-import WatchEpisode from "../VideoPlayer/WatchEpisode";
+import WatchEpisode from "../VideoPlayer/WatchEpisode"
+import History  from './History';
+import Downloads from './Downloads';
+import AccountSettings from './Accountsetting';
 
 const App = () => {
   return (
     <Routes>
       <Route path="Subscription" element={<Subscription />} />
-      <Route path="/payment" element={<PaymentPage />} />
+      <Route path="payment" element={<PaymentPage />} />
       <Route path="payment-options" element={<PaymentOptions />} />
       <Route path="Profile" element={<Profile />} />
       <Route path="login" element={<Login />} />
       <Route path="FanRankings" element={<FanRankings />} />
       <Route path='PrankEpisodes' element={<PrankEpisodes/>}/>
       <Route path='WatchEpisode' element={<WatchEpisode/>}/>
-      
+      <Route path='History' element={<History/>}/>
+      <Route path='Downloads' element={<Downloads/>}/>
+      <Route path='Accountsetting' element={<AccountSettings/>}/>
     </Routes>
   );
 };
